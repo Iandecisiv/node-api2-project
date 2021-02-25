c54c51cf8f0f66dc970432bc2609b3669c5ebcc9
Ian-Tucker <iandecisiv@gmail.com>
2021-02-24 22:59:04 -0600

    MVP

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // require your server and launch it here
+const server = require('./api/server.js');
+
+const port = process.env.PORT || 9000;
+
+server.listen(port, () => {
+    console.log('\n Server is running on localhost:9000 \n');
+});
+
+// \n is breaking the console log for ease of reading
